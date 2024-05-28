b0ed0d062fdff116ad323350b003380ba000eef5
KsaweryAiR <85024534+KsaweryAiR@users.noreply.github.com>
2024-05-29 01:02:27 +0200

    new ai

--- NBA.py
+++ NBA.py
@@ -1,100 +1,98 @@
 import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy.stats import spearmanr
-
-
-def plot_correlation_pearson_bar_sorted(data, features, year, value):
-    data = data[data['YEAR'] != year].copy()
-    correlation_values = {}
-    for feature in features:
-        correlation, _ = spearmanr(data[feature], data['Result'])
-        correlation_values[feature] = correlation
-
-    sorted_correlation = sorted(correlation_values.items(), key=lambda x: x[1], reverse=True)
-    sorted_features = [x[0] for x in sorted_correlation]
-    sorted_correlation_values = [x[1] for x in sorted_correlation]
-
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x=sorted_features, y=sorted_correlation_values, color='skyblue')
-    plt.title('Correlation between Features and Result (Sorted)')
-    plt.xlabel('Feature')
-    plt.ylabel('Correlation with Result')
-    plt.xticks(rotation=45, ha='right')
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.tight_layout()
-    plt.show()
-
-    significant_features = [feature for feature, correlation in sorted_correlation if correlation >= value]
-    return significant_features
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+import json
 
 
-def plot_correlation_bar_sorted(data, features, year, value):
-    data = data[data['YEAR'] != year].copy()
-    correlation_matrix = data[features + ['Result']].corr()
+def generate_all_nba_json(first_team, second_team, third_team, first_rookie_team, second_rookie_team, filename):
+    data = {
+        "first all-nba team": first_team,
+        "second all-nba team": second_team,
+        "third all-nba team": third_team,
+        "first rookie all-nba team": first_rookie_team,
+        "second rookie all-nba team": second_rookie_team
+    }
+
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
+
+
+
+def remove_duplicate_players(first_team, second_team, third_team):
+    all_players = {}
+    for player, frequency in first_team.items():
+        all_players[player] = max(all_players.get(player, 0), frequency)
+    for player, frequency in second_team.items():
+        all_players[player] = max(all_players.get(player, 0), frequency)
+    for player, frequency in third_team.items():
+        all_players[player] = max(all_players.get(player, 0), frequency)
+
+    def filter_team(team):
+        return {player: frequency for player, frequency in team.items() if frequency == all_players[player]}
+
+    filtered_first_team = filter_team(first_team)
+    filtered_second_team = filter_team(second_team)
+    filtered_third_team = filter_team(third_team)
+
+    return filtered_first_team, filtered_second_team, filtered_third_team
+
+def filter_top_players_by_year(data, stats, percentage):
+    top_players_per_year = []
+    start_year = data['YEAR'].min()
+    end_year = data['YEAR'].max()
+    for year in range(start_year, end_year + 1):
+        filtered_data = data[data['YEAR'] == year].copy()
+        if not filtered_data.empty:
+            filtered_data['Average_Stats'] = filtered_data[stats].mean(axis=1)
+            sorted_data = filtered_data.sort_values(by='Average_Stats', ascending=False).copy()
+            top_percentage = int(len(sorted_data) * percentage)
+            top_players = sorted_data.head(top_percentage).copy()
+            top_players_per_year.append(top_players)
+    top_players_all_years = pd.concat(top_players_per_year).reset_index(drop=True)
+    return top_players_all_years
+
+
+
+def corrlation_cal(data, features, year, value):
+    data_00_22 = data[data['YEAR'] != year].copy()
+    correlation_matrix = data_00_22[features + ['Result']].corr()
     sorted_correlation = correlation_matrix['Result'].drop('Result').sort_values(ascending=False)
-
-    plt.figure(figsize=(10, 6))
-    sorted_correlation.plot(kind='bar', color='skyblue')
-    plt.title('Correlation between Features and Result (Sorted)')
-    plt.xlabel('Feature')
-    plt.ylabel('Correlation with Result')
-    plt.xticks(rotation=45, ha='right')
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.tight_layout()
-    plt.show()
 
     significant_features = sorted_correlation[sorted_correlation >= value].index.tolist()
     return significant_features
 
 
-def corr_map(df, f):
-
-    data_for_corr = df[f]
-    corr = data_for_corr.corr()
-    plt.figure(figsize=(30, 15))
-    sns.heatmap(corr, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
-    plt.title('Map COR')
-    plt.show()
-
 def filter_G_position(data):
     return data[data['Position'].str.contains('G') | (data['Position'] == 'NONE')]
+
+
 def filter_C_position(data):
     return data[data['Position'].str.contains('C') | (data['Position'] == 'NONE')]
+
+
 def filter_F_position(data):
     return data[data['Position'].str.contains('F') | (data['Position'] == 'NONE')]
 
 
 def evaluate(models, BIG_DATA):
-
     total_player_count_100 = {}
     total_player_count_60 = {}
     total_player_count_20 = {}
 
     for model in models:
-        print('model:', model)
         for data in BIG_DATA:
             X_train, y_train, X_test, y_test, player_data = data
             model.fit(X_train, y_train)
             y_pred = model.predict(X_test)
-            player_count_100 = {} #first team
-            player_count_60 = {} #second team
-            player_count_20 = {} #third team
+            player_count_100 = {}  # first team
+            player_count_60 = {}  # second team
+            player_count_20 = {}  # third team
 
             for player_name, pred_label in zip(player_data['PLAYER_NAME'], y_pred):
                 if pred_label == 3:
@@ -103,10 +101,6 @@ def evaluate(models, BIG_DATA):
                     player_count_60[player_name] = player_count_60.get(player_name, 0) + 1
                 if pred_label == 1:
                     player_count_20[player_name] = player_count_20.get(player_name, 0) + 1
-            print("player_count_3: ", player_count_100)
-            print("player_count_2: ", player_count_60)
-            print("player_count_1: ", player_count_20)
-            print(" ")
 
             for player_name, count in player_count_100.items():
                 total_player_count_100[player_name] = total_player_count_100.get(player_name, 0) + count
@@ -123,7 +117,9 @@ def evaluate(models, BIG_DATA):
                              sorted(total_player_count_20.items(), key=lambda item: item[1], reverse=True)}
 
 
+
     return total_player_count_100, total_player_count_60, total_player_count_20
+
 
 def make_collection(data, year, target, features):
     train_data = data[data['YEAR'] != year].copy()
@@ -140,7 +136,8 @@ def make_collection(data, year, target, features):
     X_test = test_data[features]
     y_test = test_data[target]
 
-    return X_train, y_train, X_test,  y_test, player_data
+    return X_train, y_train, X_test, y_test, player_data
+
 
 def All_NBA_TEAM(stats_players, result_data, year):
     merged_data = pd.merge(stats_players, result_data[['PLAYER_NAME', 'YEAR', 'Result']], on=['PLAYER_NAME', 'YEAR'],
@@ -151,70 +148,58 @@ def All_NBA_TEAM(stats_players, result_data, year):
     merged_data['Position'] = merged_data['Position'].fillna('NONE')
     merged_data.drop(columns=['TEAM_ID'], inplace=True)
     features = ['AGE', 'GP', 'W', 'L', 'W_PCT', 'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM',
-                       'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'BLKA', 'PF', 'PFD', 'PTS',
-                       'PLUS_MINUS', 'NBA_FANTASY_PTS', 'DD2', 'TD3', 'WNBA_FANTASY_PTS', 'FG_PCT', 'FG3_PCT', 'FT_PCT']
-
-
-    features_G = ['PTS', 'AST', 'TOV', 'L', 'BLK', 'STL'] # idealnie
-    features_C = ['REB', 'PTS', 'GP' , 'PLUS_MINUS', 'STL', 'REB',]
-    features_F = ['PTS', 'PLUS_MINUS', 'AST', 'GP', 'STL', 'REB', 'BLK', 'L' ]# idealnie
+                'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'BLKA', 'PF', 'PFD', 'PTS',
+                'PLUS_MINUS', 'NBA_FANTASY_PTS', 'DD2', 'TD3', 'WNBA_FANTASY_PTS', 'FG_PCT', 'FG3_PCT', 'FT_PCT']
 
     target = 'Result'
+    MAIN_FEATURES = corrlation_cal(merged_data, features, year, 0.2)
 
-    filtered_stats_F = filter_F_position(merged_data)
-    filtered_stats_C = filter_C_position(merged_data)
-    filtered_stats_G = filter_G_position(merged_data)
+    filter_data  = filter_top_players_by_year(merged_data, MAIN_FEATURES, 0.4)
 
-    features_F = plot_correlation_bar_sorted(filtered_stats_F, features, year, 0.3)
-    features_C = plot_correlation_bar_sorted(filtered_stats_C, features, year, 0.3)
-    features_G = plot_correlation_bar_sorted(filtered_stats_G, features, year, 0.3)
+    filtered_stats_F = filter_F_position(filter_data)
+    filtered_stats_C = filter_C_position(filter_data)
+    filtered_stats_G = filter_G_position(filter_data)
 
-    #corr_map(filtered_stats_F, features_F)
-    BIG_DATA_F = make_collection(filtered_stats_F, year, target, features_F)#X_trainF, y_trainF, X_testF, y_testF, player_dataF
-    BIG_DATA_C = make_collection(filtered_stats_C, year, target, features_C)#X_trainC, y_trainC, X_testC, y_testC, player_dataC
-    BIG_DATA_G = make_collection(filtered_stats_G, year, target, features_G)#X_trainG, y_trainG, X_testG, y_testG, player_dataG
+    features_F = corrlation_cal(filtered_stats_F, features, year, 0.3)
+    features_C = corrlation_cal(filtered_stats_C, features, year, 0.25)
+    features_G = corrlation_cal(filtered_stats_G, features, year, 0.1)
 
-    BIG_DATA = [BIG_DATA_F, BIG_DATA_C , BIG_DATA_G]
-    #BIG_DATA = [BIG_DATA_F]
+
+    BIG_DATA_F = make_collection(filtered_stats_F, year, target,
+                                 features_F)  # X_trainF, y_trainF, X_testF, y_testF, player_dataF
+    BIG_DATA_C = make_collection(filtered_stats_C, year, target,
+                                 features_C)  # X_trainC, y_trainC, X_testC, y_testC, player_dataC
+    BIG_DATA_G = make_collection(filtered_stats_G, year, target,
+                                 features_G)  # X_trainG, y_trainG, X_testG, y_testG, player_dataG
+
+    BIG_DATA = [BIG_DATA_F, BIG_DATA_C, BIG_DATA_G]
+
     models = [
-        LogisticRegression(random_state=42), #4,0,1  #ten
-        KNeighborsClassifier(n_neighbors=8, weights="uniform"), #3,0,0#c
-        #DecisionTreeClassifier(random_state=42),#1,0,0
-        RandomForestClassifier(random_state=42, n_estimators=100),#3,0,0#c
-        #GradientBoostingClassifier(random_state=42, n_estimators=100), #2,0,0#ten
-        AdaBoostClassifier(random_state=42), #ten #1,1,0
-        SVC(kernel='linear'), #3,0,0
-        #GaussianNB(), #2,0,0
-        MLPClassifier(random_state=42, max_iter=1000) #3,1,1
+        LogisticRegression(solver='liblinear'),
+        KNeighborsClassifier(n_neighbors=8, weights="uniform"),
+        RandomForestClassifier(random_state=42, n_estimators=100),
+        GradientBoostingClassifier(random_state=42, n_estimators=100),
+        MLPClassifier(random_state=42, max_iter=1000),
+        AdaBoostClassifier(random_state=42, n_estimators=300),
     ]
 
-    total_player_count_100, total_player_count_60, total_player_count_20 = evaluate(models, BIG_DATA)
+    first_team, second_team, third_team = evaluate(models, BIG_DATA)
 
-    print("FIRST TEAM")
-    print(list(total_player_count_100.items())[:5])
-    print("SECOND TEAM")
-    print(list(total_player_count_60.items())[:5])
-    print("THIRD TEAM")
-    print(list(total_player_count_20.items())[:5])
+    filtered_first_team, filtered_second_team, filtered_third_team = remove_duplicate_players(first_team, second_team,
+                                                                                              third_team)
+    first_ALLteam = list(filtered_first_team.keys())[:5]
+    second_ALLteam = list(filtered_second_team.keys())[:5]
+    third_ALLteam = list(filtered_third_team.keys())[:5]
 
-    data_2023 = merged_data[merged_data['YEAR'] == year]
+    print("FIRST ALL TEAM")
+    print(first_ALLteam)
+    print("SCOND ALL TEAM")
+    print(second_ALLteam)
+    print("THIRD ALL TEAM")
+    print(third_ALLteam)
 
-    # Wyświetlenie graczy z różnymi wartościami wynikowymi
-    first_team = set(data_2023[data_2023['Result'] == 3]['PLAYER_NAME'])
-    second_team = set(data_2023[data_2023['Result'] == 2]['PLAYER_NAME'])
-    third_team = set(data_2023[data_2023['Result'] == 1]['PLAYER_NAME'])
+    return first_ALLteam, second_ALLteam, third_ALLteam
 
-    top_5_first_team = set(list(total_player_count_100.keys())[:5])
-    top_5_second_team = set(list(total_player_count_60.keys())[:5])
-    top_5_third_team = set(list(total_player_count_20.keys())[:5])
-
-    common_in_first = first_team.intersection(top_5_first_team)
-    common_in_second = second_team.intersection(top_5_second_team)
-    common_in_third = third_team.intersection(top_5_third_team)
-
-    print("Liczba dopasowań w pierwszej drużynie:", len(common_in_first))
-    print("Liczba dopasowań w drugiej drużynie:", len(common_in_second))
-    print("Liczba dopasowań w trzeciej drużynie:", len(common_in_third))
 
 def All_NBA_ROOKIE_TEAM(stats_players, result_data, year):
     stats_players.sort_values(by=['PLAYER_NAME', 'YEAR'], inplace=True)
@@ -235,11 +220,9 @@ def All_NBA_ROOKIE_TEAM(stats_players, result_data, year):
                 'PLUS_MINUS', 'NBA_FANTASY_PTS', 'DD2', 'TD3', 'WNBA_FANTASY_PTS', 'FG_PCT', 'FG3_PCT', 'FT_PCT']
     target = 'Result'
 
-    featuresOPT = plot_correlation_bar_sorted(merged_data, features, year, 0.35)
-    #featuresOPT = plot_correlation_pearson_bar_sorted(merged_data, features, year, 0.3)
-    #print(featuresOPT)
-    corr_map( merged_data, featuresOPT)
-    BIG_DATA = make_collection(merged_data, year, target, featuresOPT)#X_trainF, y_trainF, X_testF, y_testF, player_dataF
+    featuresOPT = corrlation_cal(merged_data, features, year, 0.35)
+
+    BIG_DATA = make_collection(merged_data, year, target, featuresOPT)  # X_trainF, y_trainF, X_testF, y_testF, player_dataF
     BIG_DATA = [BIG_DATA]
     models = [
         KNeighborsClassifier(n_neighbors=20, weights="uniform"),
@@ -249,41 +232,20 @@ def All_NBA_ROOKIE_TEAM(stats_players, result_data, year):
         GaussianNB()
     ]
 
-    total_player_count_100, total_player_count_60, total_player_count_20 = evaluate(models, BIG_DATA)
+    _, FIRST, SECOND = evaluate(models, BIG_DATA)
 
-    print("FIRST TEAM")
-    print(list(total_player_count_100.items())[:5])
-    print("SECOND TEAM")
-    print(list(total_player_count_60.items())[:5])
-    print("THIRD TEAM")
-    print(list(total_player_count_20.items())[:5])
+    rookie_first_team = list(FIRST.keys())[:5]
+    rookie_second_team = list(SECOND.keys())[:5]
 
-    # Wybór danych dla roku 2023
-    data_2023 = merged_data[merged_data['YEAR'] == 2023]
+    # print("FIRST ROOKIE TEAM")
+    # print(rookie_first_team)
+    # print("SECOND ROOKIE TEAM")
+    # print(rookie_second_team)
 
-    # Wyświetlenie graczy z różnymi wartościami wynikowymi
-    first_team = set(data_2023[data_2023['Result'] == 3]['PLAYER_NAME'])
-    second_team = set(data_2023[data_2023['Result'] == 2]['PLAYER_NAME'])
-    third_team = set(data_2023[data_2023['Result'] == 1]['PLAYER_NAME'])
-
-    # Pobranie pierwszych pięciu kluczy (imion graczy) z każdego słownika
-    top_5_first_team = set(list(total_player_count_100.keys())[:5])
-    top_5_second_team = set(list(total_player_count_60.keys())[:5])
-    top_5_third_team = set(list(total_player_count_20.keys())[:5])
-
-    # Liczenie dopasowań
-    common_in_first = first_team.intersection(top_5_first_team)
-    common_in_second = second_team.intersection(top_5_second_team)
-    common_in_third = third_team.intersection(top_5_third_team)
-
-    # Wyświetlenie liczby dopasowań
-    print("Liczba dopasowań w pierwszej drużynie:", len(common_in_first))
-    print("Liczba dopasowań w drugiej drużynie:", len(common_in_second))
-    print("Liczba dopasowań w trzeciej drużynie:", len(common_in_third))
-
+    return rookie_first_team, rookie_second_team
 
 stats_players = pd.read_csv('ALL_position.csv')
-result_data = pd.read_csv('result_ALL_NBA_Team.csv')
+result_data = pd.read_csv('result_ALL.csv')
 result_data_rookie = pd.read_csv('result_rookie.csv')
 
 numeric_columns = ['AGE', 'GP', 'W', 'L', 'W_PCT', 'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM',
@@ -298,5 +260,7 @@ stats_players[numeric_columns] = scaler.fit_transform(stats_players[numeric_colu
 scaler = MinMaxScaler()
 stats_players[percentage_columns] = scaler.fit_transform(stats_players[percentage_columns])
 
-All_NBA_TEAM(stats_players, result_data, 2023)
-#All_NBA_ROOKIE_TEAM(stats_players, result_data_rookie, 2023)
+first_ALL_NBA, second_ALL_NBA, third_ALL_NBA = All_NBA_TEAM(stats_players, result_data, 2023)
+rookie_first_NBA_team, rookie_second_NBA_team = All_NBA_ROOKIE_TEAM(stats_players, result_data_rookie, 2023)
+
+generate_all_nba_json(first_ALL_NBA, second_ALL_NBA, third_ALL_NBA, rookie_first_NBA_team, rookie_second_NBA_team, 'nba_teams.json')
